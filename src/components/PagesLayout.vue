6431efabde9b5d56e09971e6bd85624fd659f4ea
yuu-eguci <yuu.eguci@gmail.com>
2020-01-14 10:26:15 +0900

    だけど、 URL ごとに違う外部 css を使う方法がわからない。
    だけど、 URL ごとに違う外部 css を使う方法がわからない。

--- src/components/PagesLayout.vue
+++ src/components/PagesLayout.vue
@@ -10,5 +10,11 @@
 </script>
 
 <style scoped>
+  /* FIXME: こう書くと pageslayout の .pagename にだけ適用される。でもこれを外部ファイルから読み込む方法がわからない。 */
+  .pagename {
+    background-color: aquamarine;
+  }
 
+  /* こう import するとこのコンポーネントの他…… Index.vue とかでも適用されちゃう。なんでやねんバグでは? */
+  /* @import '../assets/pageslayout.css'; */
 </style>
